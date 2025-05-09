7fbb7986a67f693ed4135d7d7cdf3c392520f6a1
DevKeria <keriawiley@yahoo.com>
2025-05-09 23:39:31 +0000

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,81 +1,10 @@
-import "../style/index.css";
-
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
-function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-
-  let cover = variables.includeCover
-    ? `<div class="cover"><img src="${variables.background}" /></div>`
-    : "<div class='cover'></div>";
-
-  document.querySelector("#widget_content").innerHTML = `
-        <div class="widget">
-            ${cover}
-            <img src="${variables.avatarURL}" class="photo" />
-            <h1>${variables.name} ${variables.lastName}</h1>
-            <h2>${variables.role}</h2>
-            <h3>${variables.city}, ${variables.country}</h3>
-            <ul class="${variables.socialMediaPosition}">
-                <li><a href="${
-                  variables.twitter
-                    ? "https://twitter.com/" + variables.twitter
-                    : "#"
-                }"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="${
-                  variables.github
-                    ? "https://github.com/" + variables.github
-                    : "#"
-                }"><i class="fab fa-github"></i></a></li>
-                <li><a href="${
-                  variables.linkedin
-                    ? "https://linkedin.com/in/" + variables.linkedin
-                    : "#"
-                }"><i class="fab fa-linkedin"></i></a></li>
-                <li><a href="${
-                  variables.instagram
-                    ? "https://instagram.com/" + variables.instagram
-                    : "#"
-                }"><i class="fab fa-instagram"></i></a></li>
-            </ul>
-        </div>
-    `;
-}
-
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
-  window.variables = {
-    // if includeCover is true the algorithm should show the cover image
+  // Initial variables setup
+  const variables = {
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
     socialMediaPosition: "position-left",
-    // social media usernames
     twitter: null,
     github: null,
     linkedin: null,
@@ -86,22 +15,61 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); // render the card for the first time
 
+  // Function to render the profile card
+  function render(variables) {
+    console.log("Rendering with variables:", variables);
+
+    let cover = variables.includeCover
+      ? `<div class="cover"><img src="${variables.background}" /></div>`
+      : "<div class='cover'></div>";
+
+    document.querySelector("#profile-card").innerHTML = `
+      <div class="widget">
+        ${cover}
+        <img src="${variables.avatarURL}" class="photo" />
+        <h1>${variables.name || "First Name"} ${variables.lastName ||
+      "Last Name"}</h1>
+        <h2>${variables.role || "Role"}</h2>
+        <h3>${variables.city || "City"}, ${variables.country || "Country"}</h3>
+        <ul class="${variables.socialMediaPosition}">
+          <li><a href="${
+            variables.twitter ? "https://twitter.com/" + variables.twitter : "#"
+          }"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="${
+            variables.github ? "https://github.com/" + variables.github : "#"
+          }"><i class="fab fa-github"></i></a></li>
+          <li><a href="${
+            variables.linkedin
+              ? "https://linkedin.com/in/" + variables.linkedin
+              : "#"
+          }"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="${
+            variables.instagram
+              ? "https://instagram.com/" + variables.instagram
+              : "#"
+          }"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    `;
+  }
+
+  // Initial render
+  render(variables);
+
+  // Add event listeners to update the profile card on input change
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
-      let values = {};
-      values[attribute] =
-        this.value == "" || this.value == "null"
-          ? null
-          : this.value == "true"
-          ? true
-          : this.value == "false"
-          ? false
-          : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+      const attribute = e.target.getAttribute("for");
+      let value = this.value;
+
+      // Convert value to appropriate type
+      if (value === "true") value = true;
+      else if (value === "false") value = false;
+      else if (value === "") value = null;
+
+      variables[attribute] = value;
+      render(variables);
     });
   });
 };
